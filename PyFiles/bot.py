b62a4d769ee32902ffb5645fe3dce9adacb2bb97
Tamerlan926 <tamerlanshakri@gmail.com>
2026-05-21 11:58:25 +0300

    Update bot.py

--- PyFiles/bot.py
+++ PyFiles/bot.py
@@ -1,11 +1,13 @@
 import telebot
 import config
 import db
-import handles
+import handlers
+
+db.init_db()
 
 bot = telebot.TeleBot(config.TOKEN)
-handles.register_handlers(bot)
+
+handlers.register_handlers(bot)
 
 if __name__ == "__main__":
-    db.init_db()
     bot.infinity_polling()
