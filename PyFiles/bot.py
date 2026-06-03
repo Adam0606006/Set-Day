717362b160aa3d4b60639b3c96fdeec46c0d5012
Adam0606006 <adamtorshoev123@gmail.com>
2026-06-03 10:06:51 +0300

    Update bot.py

--- PyFiles/bot.py
+++ PyFiles/bot.py
@@ -4,10 +4,11 @@ import db
 import handlers
 
 db.init_db()
+db.load_test_data()
 
 bot = telebot.TeleBot(config.TOKEN)
 
 handlers.register_handlers(bot)
 
 if __name__ == "__main__":
-    bot.infinity_polling()
+    bot.polling()
